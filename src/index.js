8633ab1cc96e96a5cc837bd294a4c6de011a75e0
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-09-14 14:42:47 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+ function buildToDo(todo){
+  let list = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = `x`
+  list.textContent = `${todo}`
+  list.appendChild(btn)
+  console.log(list)
+  document.querySelector('#tasks').appendChild(list)
+ }
+
+ function handleDelete(e){
+  e.target.parentNode.remove()
+ }
